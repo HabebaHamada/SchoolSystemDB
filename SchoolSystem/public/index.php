4acacbc478c7bad53c0632ffe9b199bb3f17befc
Habeba Hamada <99296931+HabebaHamada@users.noreply.github.com>
2025-06-15 15:56:49 +0300

    Update index.php

--- SchoolSystem/public/index.php
+++ SchoolSystem/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../database/Database.php';
+require_once __DIR__ .'/../models/classModel.php';
 
 $DataBase = new Database();
 
@@ -13,12 +14,12 @@ else{
     echo "successfull connection to database\n";
 }
 
-$query = "SELECT * FROM Class";
-$result = $db->prepare($query);
-$result->execute();
-
-$rowCount = $result->rowCount();
-echo "Number of rows in CLASS table: " . $rowCount . "\n";
+$classModel = new ClassModel($db);  
+/*$classModel->createClass("502");*/
 
 
+$classes = $classModel->getAllClasses();
 
+ foreach ($classes as $classRow) {
+    echo "Class ID: " . $classRow['ID'] . ", Class Name: " . $classRow['Name'] . "\n";
+ }
