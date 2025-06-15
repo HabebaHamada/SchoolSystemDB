19c385055b033a693e53da747dcfbd282186a783
Habeba Hamada <99296931+HabebaHamada@users.noreply.github.com>
2025-06-15 10:21:59 +0300

    Update classModel.php

--- SchoolSystem/models/classModel.php
+++ SchoolSystem/models/classModel.php
@@ -12,41 +12,96 @@ class ClassModel extends BaseModel {
     U -> UPDATE
     D -> DELETE */
 
-    public function getAllClasses(): bool{
+    public function getAllClasses(): array{
+        
         $query = "SELECT * FROM " . $this->table_name;
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        return $stmt;
+
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        
+        } catch(PDOException $e){
+            
+            error_log("Error Reading all Classes: " . $e->getMessage());
+            return [];
+        }
     }
 
-    public function getClassById($id): bool|PDOStatement{
+    public function getClassById(int $id):array|bool {
+        
         $query = "SELECT * FROM " . $this->table_name . " WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-        return $stmt;
+
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $row = $stmt->fetch();
+            return $row;
+        } catch(PDOException $e){
+             error_log("Error reading class: " . $e->getMessage());
+             return false;          
+        }
     }
 
-    public function createClass($id,$name): void {
+    public function createClass($id,$name) {
         $query = "INSERT INTO " . $this->table_name . " (id, name) VALUES (:id, :name)";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':name', $name);
-        $stmt->execute();
+
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':name', $name);
+            $stmt->execute();
+        } catch(PDOException $e){
+            error_log("Error creating class: " . $e->getMessage());
+            return false;
+        }
     }
 
-    public function updateClass($id, $name): void {
+    public function updateClass($id, $name){
         $query = "UPDATE " . $this->table_name . " SET name = :name WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':name', $name);
-        $stmt->execute();
+
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':name', $name);
+            $stmt->execute();
+        } catch(PDOException $e){
+            error_log("Error updating class: " . $e->getMessage());
+            return false;
+        }
     }
 
-    public function deleteClass($id): void {
+    public function deleteClass($id){
         $query = "DELETE FROM " . $this->table_name . " WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
+        
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+         } catch(PDOException $e){
+            error_log("Error deleting  class: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function getStudents($classID) 
+    {
+          $query = "SELECT Class.Name AS className, Student.Name AS studentName 
+                   FROM " . $this->table_name 
+                   . " LEFT JOIN Student ON Class.ID= :ClassID 
+                   ORDER BY className";
+
+        try{
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':ClassID', $classID);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        
+        } catch(PDOException $e){
+            
+            error_log("this class has no students : " . $e->getMessage());
+            return [];
+        }
     }
 }
